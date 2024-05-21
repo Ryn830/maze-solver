26ca0b9b1a768da21f83f5a67edde7f238232d33
Ryan Yee <ryn830@gmail.com>
2024-05-21 10:30:39 -0700

    Separate the window class from main

---
+++ window.py
@@ -0,0 +1,25 @@
+from tkinter import Tk, BOTH, Canvas
+
+
+class Window:
+    def __init__(self, width, height) -> None:
+        self.root_widget = Tk()
+        self.root_widget.title = "Maze Solver"
+        self.root_widget.protocol("WM_DELETE_WINDOW", self.close)
+
+        self.canvas = Canvas(self.root_widget, bg="black", height=height, width=width)
+        self.canvas.pack(fill=BOTH, expand=1)
+
+        self.status_running = False
+
+    def redraw(self):
+        self.root_widget.update_idletasks()
+        self.root_widget.update()
+
+    def wait_for_close(self):
+        self.status_running = True
+        while self.status_running:
+            self.redraw()
+
+    def close(self):
+        self.status_running = False
