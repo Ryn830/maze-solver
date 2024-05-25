3fe9d34cf1bc867764036ac3a9393f8c025f48f9
Ryan Yee <ryn830@gmail.com>
2024-05-24 17:44:27 -0700

    Add another mock to test case

--- maze_test.py
+++ maze_test.py
@@ -9,10 +9,18 @@ class Tests(unittest.TestCase):
         cols = 12
         rows = 10
 
-        maze = Maze(0, 0, cols, rows, 10, 10)
+        maze_1 = Maze(0, 0, cols, rows, 10, 10)
 
-        self.assertEqual(len(maze.cells), cols)
-        self.assertEqual(len(maze.cells[0]), rows)
+        self.assertEqual(len(maze_1.cells), cols)
+        self.assertEqual(len(maze_1.cells[0]), rows)
+
+        cols = 20
+        rows = 15
+
+        maze_2 = Maze(0, 0, cols, rows, 10, 10)
+
+        self.assertEqual(len(maze_2.cells), cols)
+        self.assertEqual(len(maze_2.cells[0]), rows)
 
 
 if __name__ == "__main__":
